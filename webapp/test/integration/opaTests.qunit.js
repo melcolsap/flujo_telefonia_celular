c53fcc02c13671c3d4768f1a239f08276ca54379
Carlos Sanchez <carlos.sanchez@melcol.com.co>
2026-06-05 14:03:21 +0000

    Primer Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["co/mitsubishi/flujotelefoniacelular/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"co/mitsubishi/flujotelefoniacelular/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
